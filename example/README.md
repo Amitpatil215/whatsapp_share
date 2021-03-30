45bb1b18f933136c866244ff5d75fa85f7c23aab
Amit Patil <amitgpatil215@gmail.com>
2021-03-30 10:36:11 +0530

    rename plugin

--- example/README.md
+++
@@ -1,8 +0,0 @@
-# flutter_share_example
-
-Demonstrates how to use the flutter_share plugin.
-
-## Getting Started
-
-For help getting started with Flutter, view our online
-[documentation](https://flutter.io/).
--- ios/Classes/FlutterSharePlugin.m
+++
@@ -1,8 +0,0 @@
-#import "FlutterSharePlugin.h"
-#import <flutter_share/flutter_share-Swift.h>
-
-@implementation FlutterSharePlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFlutterSharePlugin registerWithRegistrar:registrar];
-}
-@end
